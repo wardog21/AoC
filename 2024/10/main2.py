56747ce6b00490a7e8ace12c3332b3609eda2be1
Marco <wardog21hd@gmail.com>
2024-12-11 01:16:02 +0100

    Day 10

---
+++ 2024/10/main2.py
@@ -0,0 +1,44 @@
+with open("10/input.txt") as file:
+    topo = file.readlines()
+
+nodes = []
+
+for y in range(len(topo)):
+    for x in range(len(topo[0])-1):
+        if topo[y][x] == '.':
+            nodes.append([-1,[]])
+        elif topo[y][x] == '9':
+            nodes.append([9,1])
+        else:
+            nodes.append([int(topo[y][x]),[]])
+            if x > 0:
+                if topo[y][x-1] != '.':
+                    if int(topo[y][x-1]) == int(topo[y][x])+1:
+                        nodes[-1][1].append(y*(len(topo[0])-1)+x-1)
+            if y > 0:
+                if topo[y-1][x] != '.':
+                    if int(topo[y-1][x]) == int(topo[y][x])+1:
+                        nodes[-1][1].append((y-1)*(len(topo[0])-1)+x)
+            if x < len(topo[0])-2:
+                if topo[y][x+1] != '.':
+                    if int(topo[y][x+1]) == int(topo[y][x])+1:
+                        nodes[-1][1].append(y*(len(topo[0])-1)+x+1)
+            if y < len(topo)-1:
+                if topo[y+1][x] != '.':
+                    if int(topo[y+1][x]) == int(topo[y][x])+1:
+                        nodes[-1][1].append((y+1)*(len(topo[0])-1)+x)
+
+for height in range(8,0,-1):
+    for node in nodes:
+        if node[0] == height:
+            temp = 0
+            for nodeId in node[1]:
+                temp += nodes[nodeId][1]
+            node[1] = temp
+
+trailheads = 0
+for node in nodes:
+    if node[0] == 0:
+        for nodeId in node[1]:
+            trailheads += nodes[nodeId][1]
+print(trailheads)
