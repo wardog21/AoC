3e47cae9c50bde68549ff2e30ce0bc249ea47d80
Marco <wardog21hd@gmail.com>
2024-12-18 00:34:28 +0100

    Day 17 Part 2

--- 2024/17/main2.py
+++ 2024/17/main2.py
@@ -1,37 +1,34 @@
-# # # not working
+command = [2,4,1,5,7,5,4,5,0,3,1,6,5,5,3,0]
 
-# import functools
-# command = [2,4,1,5,7,5,4,5,0,3,1,6,5,5,3,0]
+def calc(regA):
+    output = []
+    while True:
+        regB = regA % 8
+        regB = regB ^ 5
+        regC = int( regA // (1 << regB) )
+        regB = regB ^ regC
+        regA = int( regA // 8 )
+        regB = regB ^ 6
+        output.append(regB % 8)
+        if regA == 0:
+            break
+    return output
 
-# @functools.cache
-# def calc(regA):
-#     regB = regA % 8
-#     regB = regB ^ 5
-#     regC = int( regA // (1 << regB) )
-#     regB = regB ^ regC
-#     regA = int( regA // 8 )
-#     regB = regB ^ 6
-#     output = regB % 8
-#     return regA, output
+queue = [(15, 0)]
+potential_As = []
 
-# for i in range(10000000):
-#     output = []
-#     found = False
+while queue:
+    i, a = queue.pop(0)
+    if i < 0:
+        continue
+    for o in range(8):
+        test_a = (a << 3) + o
+        output = calc(test_a)
+        if not output == command[i:]:
+            continue
+        if i == 0:
+            potential_As.append(test_a)
+        queue.append((i-1, test_a))
 
-#     while True:
-#         i, out = calc(i)
-#         output.append(out)
-
-#         if len(output) > 0:
-#             found = True
-#             for i in range(len(output)):
-#                 if command[i] != output[i]:
-#                     found = False
-#                     break
-#             if not found: continue
-#         if len(output) == len(command) and found:
-#             break
-    
-#     if found:
-#         print(i)
-#         break
+potential_As.sort()
+print(potential_As[0])
