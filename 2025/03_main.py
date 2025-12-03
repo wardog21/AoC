2b2e4374c3f2cadd568a8b6c5a90939f54fc3aea
Marco <wardog21hd@gmail.com>
2025-12-03 18:15:53 +0100

    cleanup

--- 2025/03_main.py
+++ 2025/03_main.py
@@ -8,26 +8,17 @@ ROOT_DIR = Path(__file__).parent
 with open(ROOT_DIR / "03_data.txt") as file:
     lines = file.readlines()
 
-for line in lines:
-    batteries = [int(b) for b in re.findall(r"(\d)",line)]
-    
-    # code
-    high = max(batteries)
-    posHigh = batteries.index(high)
-    if len(batteries) == posHigh+1:
-        secHigh = max(batteries[:-1])
-        code += 10*secHigh+high
-    else:
-        secHigh = max(batteries[posHigh+1:])
-        code += 10*high+secHigh
-    
-    # code2
-    setPos = 0
+def getJolts(batteries, amount):
     finalBatteries = []
-    while len(finalBatteries) < 12:
-        firstHigh = max(batteries[:len(batteries)-11+len(finalBatteries)])
+    while len(finalBatteries) < amount:
+        firstHigh = max(batteries[:len(batteries)-amount+1+len(finalBatteries)])
         finalBatteries.append(firstHigh)
         batteries = batteries[batteries.index(firstHigh)+1:]
-    code2 += int(''.join(map(str,finalBatteries)))
+    return int(''.join(map(str,finalBatteries)))
+
+for line in lines:
+    batteries = [int(b) for b in re.findall(r"(\d)",line)]
+    code  += getJolts(batteries, 2)
+    code2 += getJolts(batteries,12)
 
 print(code, code2)
