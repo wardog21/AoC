5d8bdbd166a76261cbdb9c3da452fc6f550f993c
Marco <wardog21hd@gmail.com>
2025-12-09 20:25:59 +0100

    working but slow

--- 2025/09_main.py
+++ 2025/09_main.py
@@ -1,6 +1,7 @@
 import re
 from pathlib import Path
 import numpy as np
+from matplotlib.path import Path as MathPath
 
 code = 0
 code2 = 0
@@ -14,11 +15,11 @@ tiles = [[int(i) for i in re.findall(r"(\d+)", line)] for line in lines]
 area = set()
 for tile in tiles:
     for tile2 in tiles:
-        area.add((abs(tile[0]-tile2[0])+1) * (abs(tile[1]-tile2[1])+1))
-code = max(area)
+        area.add(( (abs(tile[0]-tile2[0])+1) * (abs(tile[1]-tile2[1])+1), tuple(tile), tuple(tile2) ))
+area = sorted(area, key=lambda x: x[0], reverse=True)
+code = area[0][0]
 
 validTiles = set()
-invalidTiles = set()
 for i in range(len(tiles)):
     if tiles[i][0] == tiles[i-1][0]:
         x = tiles[i][0]
@@ -33,104 +34,39 @@ for i in range(len(tiles)):
         for x in range(a,b+1):
             validTiles.add((x,y))
 
-sizeY = max([i[0] for i in validTiles])
-sizeX = max([i[1] for i in validTiles])
+path = MathPath(tiles)
 
-for y in range(sizeX):
-    print(y,sizeX)
-    for x in range(sizeY):
-        if (x,y) in validTiles:
-            continue
-        if y == 0 or x == 0 or y == sizeY or x == sizeX:
-            invalidTiles.add((x,y))
-            continue
-
-        checkedTiles = set()
-        checkedTiles.add((x,y))
-        # left
-        valid = True
-        a = x
-        b = y
-        while True:
-            a -= 1
-            if (a,b) in validTiles:
-                break
-            elif (a,b) in invalidTiles:
-                valid = False
-                break
-            checkedTiles.add((a,b))
-        
-        if not valid:
-            invalidTiles.update(checkedTiles)
-            continue
-
-        # right
-        a = x
-        b = y
-        while True:
-            a += 1
-            if (a,b) in validTiles:
-                break
-            elif (a,b) in invalidTiles:
-                valid = False
-                break
-            checkedTiles.add((a,b))
-        
-        if not valid:
-            invalidTiles.update(checkedTiles)
-            continue
-
-        # up
-        a = x
-        b = y
-        while True:
-            b -= 1
-            if (a,b) in validTiles:
-                break
-            elif (a,b) in invalidTiles:
-                valid = False
-                break
-            checkedTiles.add((a,b))
-        
-        if not valid:
-            invalidTiles.update(checkedTiles)
-            continue
-
-        # down
-        a = x
-        b = y
-        while True:
-            b += 1
-            if (a,b) in validTiles:
-                break
-            elif (a,b) in invalidTiles:
-                valid = False
-                break
-            checkedTiles.add((a,b))
-        
-        if not valid:
-            invalidTiles.update(checkedTiles)
-            continue
-        else:
-            validTiles.update(checkedTiles)
-
-area2 = set()
-for tile in tiles:
-    for tile2 in tiles:
-        a = (min(tile[0],tile2[0]), min(tile[1],tile2[1]))
-        b = (max(tile[0],tile2[0]), max(tile[1],tile2[1]))
-        validArea = True
-        for x in range(a[0], b[0]+1):
-            for y in range(a[1], b[1]+1):
-                if not (x,y) in validTiles:
-                    validArea = False
-                    break
-            if not validArea:
-                break
-        
-        if validArea:
-            area2.add((b[0]-a[0]+1) * (b[1]-a[1]+1))
-
-code2 = max(area2)
+for cnt, (value,tile,tile2) in enumerate(area):
+    if cnt < 100000:
+        continue
+    print(cnt,len(area))
+    a = (min(tile[0],tile2[0]), min(tile[1],tile2[1]))
+    b = (max(tile[0],tile2[0]), max(tile[1],tile2[1]))
+    
+    checkPoints = set()
+    checkPoints2 = set([a,b,(a[0],b[1]),(b[0],a[1])])
+    for x in range(a[0], b[0]+1):
+        checkPoints.add((x,a[1]))
+        checkPoints.add((x,b[1]))
+    for y in range(a[1], b[1]+1):
+        checkPoints.add((a[0],y))
+        checkPoints.add((b[0],y))
+    
+    checkPoints = list(checkPoints.difference(validTiles))
+    checkPoints2 = list(checkPoints2.difference(validTiles))
+    
+    if len(checkPoints) == 0:
+        code2 = value
+        break
+    elif len(checkPoints2) > 0 and not all(path.contains_points(checkPoints2)):
+        continue
+    else:
+        containedPoints = path.contains_points(checkPoints)
+        for i in range(len(containedPoints)):
+            if containedPoints[i]:
+                validTiles.add(checkPoints[i])
+        if all(containedPoints):
+            code2 = value
+            break
 
 print(code,code2)
